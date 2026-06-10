8203fd1ac55d522d4246c8c28e7337423b250c9a
CaliMetaEx <emailpostspace-GitHubCaliMetaEx@yahoo.com>
2026-06-10 19:38:50 +0200

    Add package initializer (__init__.py)

---
+++ calibre_metadata_extractor/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2026 CaliMetaEx <https://github.com/CaliMetaEx>
+
+"""
+Calibre Metadata Extractor Plugin - package initializer
+This package will contain the core plugin and extraction logic.
+"""
+
+__version__ = "0.1.0"
